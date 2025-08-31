98a802f0a1951ad629b55427a9ee01ac2a0759f4
Phyopma <phyopyaemaung1@gmail.com>
2025-08-30 19:40:05 -0700

    added recursive search

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cctype>
 #include "include/RegParser.h"
+#include <filesystem>
 
 // #define DEBUG
 // #define USE_TEST
@@ -168,6 +169,25 @@ bool any_match(std::istream &in, const std::string &pattern, const std::string &
     return isMatched;
 }
 
+bool search_directory(const std::string &path, const std::string &pattern)
+{
+    bool is_found = false;
+    for (auto const &entry : std::filesystem::recursive_directory_iterator(path))
+    {
+        if (entry.is_regular_file())
+        {
+            std::ifstream file(entry.path());
+            if (!file.is_open())
+            {
+                std::cerr << "Failed to open file: " << entry.path() << std::endl;
+                continue;
+            }
+            is_found |= any_match(file, pattern, entry.path().string());
+        }
+    }
+    return is_found;
+}
+
 int main(int argc, char *argv[])
 {
     // Flush after every std::cout / std::cerr
@@ -184,44 +204,58 @@ int main(int argc, char *argv[])
     }
 
     std::string flag = argv[1];
-    std::string pattern = argv[2];
-    std::string filename;
 
-    if (flag != "-E")
+    if (flag == "-E")
+    {
+        std::string pattern = argv[2];
+        std::string filename;
+        try
+        {
+            if (argc >= 4)
+            {
+                bool is_found = false;
+                for (int i = 3; i < argc; ++i)
+                {
+                    filename = argv[i];
+                    if (!filename.empty())
+                    {
+                        std::ifstream file(filename);
+                        if (!file.is_open())
+                        {
+                            std::cerr << "Failed to open file: " << filename << std::endl;
+                            return 1;
+                        }
+                        is_found |= any_match(file, pattern, argc > 4 ? filename : "");
+                    }
+                }
+                return !is_found;
+            }
+            else
+            {
+                return any_match(std::cin, pattern) ? 0 : 1;
+            }
+        }
+        catch (const std::runtime_error &e)
+        {
+            std::cerr << e.what() << std::endl;
+            return 1;
+        }
+    }
+    else if (flag == "-r")
+    {
+        if (argc < 4)
+        {
+            std::cerr << "Expected pattern and directory" << std::endl;
+            return 1;
+        }
+        std::string pattern = argv[3];
+        std::string dir = argv[4];
+        bool found = search_directory(dir, pattern);
+        return found ? 0 : 1;
+    }
+    else
     {
         std::cerr << "Expected first argument to be '-E'" << std::endl;
-        return 1;
-    }
-
-    try
-    {
-        if (argc >= 4)
-        {
-            bool is_found = false;
-            for (int i = 3; i < argc; ++i)
-            {
-                filename = argv[i];
-                if (!filename.empty())
-                {
-                    std::ifstream file(filename);
-                    if (!file.is_open())
-                    {
-                        std::cerr << "Failed to open file: " << filename << std::endl;
-                        return 1;
-                    }
-                    is_found |= any_match(file, pattern, argc > 4 ? filename : "");
-                }
-            }
-            return !is_found;
-        }
-        else
-        {
-            return any_match(std::cin, pattern) ? 0 : 1;
-        }
-    }
-    catch (const std::runtime_error &e)
-    {
-        std::cerr << e.what() << std::endl;
         return 1;
     }
 }
